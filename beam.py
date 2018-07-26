7eb677d1a4f7297e0341bd2064ae213d06e5a972
Khakhulin <t.khakhulin@gmail.com>
2018-07-26 13:46:04 +0300

    update beam

--- beam.py
+++ beam.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 class Beam(object):
@@ -15,10 +16,9 @@ class Beam(object):
 
     def __init__(self, size, pad, bos, eos,
                  n_best=1, cuda=False,
-                 global_scorer=None,
                  min_length=0,
                  stepwise_penalty=False,
-                 block_ngram_repeat=0,
+                 block_ngram_repeat=2,
                  exclusion_tokens=set()):
 
         self.size = size
@@ -35,8 +35,13 @@ class Beam(object):
         # The outputs at each time-step.
         self.next_ys = [self.tt.LongTensor(size)
                         .fill_(pad)]
+
         self.next_ys[0][0] = bos
 
+        # for i in range(self.size):
+        #     self.next_ys[0][i] = bos
+
+        self._bos = bos
         # Has EOS topped the beam yet.
         self._eos = eos
         self.eos_top = False
@@ -60,7 +65,25 @@ class Beam(object):
 
     def get_current_state(self):
         "Get the outputs for the current timestep."
+        # return self.get_tentative_hypothesis()
         return self.next_ys[-1]
+
+    def get_tentative_hypothesis(self):
+        "Get the decoded sequence for the current timestep."
+
+        if len(self.next_ys) == 1:
+            dec_seq = self.next_ys[0].unsqueeze(1)
+        else:
+            _, keys = self.sort_scores()
+            hyps = [self.get_hypothesis(k) for k in keys]
+            hyps = [[self._bos] + h for h in hyps]
+            dec_seq = torch.from_numpy(np.array(hyps))
+        return dec_seq[...,-1]
+
+    def get_the_best_score_and_idx(self):
+        "Get the score of the best in the beam."
+        scores, ids = self.sort_scores()
+        return scores[1], ids[1]
 
     def get_current_origin(self):
         "Get the backpointers for the current timestep."
@@ -74,10 +97,14 @@ class Beam(object):
         "Update the status and check for finished or not."
         num_words = word_lk.size(1)
 
+        cur_len = len(self.next_ys)
+        if cur_len < self.min_length:
+            for k in range(len(word_lk)):
+                word_lk[k][self._eos] = -1e20
+
         # Sum the previous scores.
         if len(self.prev_ks) > 0:
             beam_lk = word_lk + self.scores.unsqueeze(1).expand_as(word_lk)
-
             # Don't let EOS have children.
             for i in range(self.next_ys[-1].size(0)):
                 if self.next_ys[-1][i] == self._eos:
@@ -87,7 +114,7 @@ class Beam(object):
                 ngrams = []
                 le = len(self.next_ys)
                 for j in range(self.next_ys[-1].size(0)):
-                    hyp, _ = self.get_hyp(le - 1, j)
+                    hyp = self.get_hypothesis(j, le - 1)
                     ngrams = set()
                     fail = False
                     gram = []
@@ -95,12 +122,13 @@ class Beam(object):
                         # Last n tokens, n = block_ngram_repeat
                         gram = (gram + [hyp[i]])[-self.block_ngram_repeat:]
                         # Skip the blocking if it is in the exclusion list
-                        if set(gram) & self.exclusion_tokens:
-                            continue
+
                         if tuple(gram) in ngrams:
                             fail = True
+                            print("Fail")
                         ngrams.add(tuple(gram))
                     if fail:
+                        print("REPEATS")
                         beam_lk[j] = -10e20
 
         else:
@@ -112,13 +140,17 @@ class Beam(object):
 
         self.all_scores.append(self.scores)
         self.scores = best_scores
-
         # bestScoresId is flattened beam x word array, so calculate which
         # word and beam each score came from
         prev_k = best_scores_id / num_words
+
+        word_ids = best_scores_id % num_words
+
+
         self.prev_ks.append(prev_k)
         self.next_ys.append(best_scores_id - prev_k * num_words)
         # End condition is when top-of-beam is EOS.
+#        for i in range(self.size):
         if self.next_ys[-1][0] == self._eos:
             self.eos_top = True
             self.all_scores.append(self.scores)
@@ -128,28 +160,22 @@ class Beam(object):
     def done(self):
         return self.eos_top #and len(self.finished) >= self.n_best
 
-
-    def get_hyp(self, k):
-        """
-        walk back to construct the full hypothesis.
-        TODO: timestep for story
-        """
-        hyp, attn = [], []
-        for j in range(len(self.prev_ks) - 1, -1, -1):
-            hyp.append(self.next_ys[j + 1][k])
-            attn.append(self.attn[j][k])
-            k = self.prev_ks[j][k]
-        return hyp[::-1], torch.stack(attn[::-1])
-
-    def get_hypothesis(self, k):
+    def get_hypothesis(self, k, timestep=None):
         """
         walk back to construct the full hypothesis.
         :param  k- the position in the beam to construct.
         :returns hypothesis
         """
+        if timestep is not None:
+            prevs = self.prev_ks[:timestep]
+
+        else:
+            prevs = self.prev_ks
         hyp = []
-        for j in range(len(self.prev_ks) - 1, -1, -1):
+        for j in range(len(prevs) - 1, -1, -1):
             hyp.append(self.next_ys[j+1][k])
             k = self.prev_ks[j][k]
+            #print(k)
+        # hyp.append(self.next_ys[0][k])
 
         return hyp[::-1]
